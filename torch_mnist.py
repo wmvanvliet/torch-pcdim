41266036299f7b3be23041eaa8f638e42f0b60ec
Marijn van Vliet <w.m.vanvliet@gmail.com>
2024-01-31 11:34:46 +0200

    Refactoring

--- torch_mnist.py
+++ torch_mnist.py
@@ -1,14 +1,12 @@
 import argparse
-from collections import OrderedDict
 
+import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
-from torch import nn
 from torchvision import datasets, transforms
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
-from torch_predcoding import InputLayer, MiddleLayer, OutputLayer
+from torch_predcoding import PCModel
 
 
 def train(args, model, device, train_loader, epoch, n_iter=20, freq=5, lr=0.01):
@@ -16,7 +14,8 @@ def train(args, model, device, train_loader, epoch, n_iter=20, freq=5, lr=0.01):
     for batch_idx, (data, target) in tqdm(
         enumerate(train_loader), total=len(train_loader), unit="batches"
     ):
-        data, target = data.to(device), target.to(device)
+        data = data.view(-1, 28 * 28).to(device)
+        target = F.one_hot(target, 10).float().to(device)
         model.reset(batch_size=len(data))
         model.clamp(data, target)
         for i in range(n_iter):
@@ -33,11 +32,12 @@ def test(model, device, test_loader, n_iter=20):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+            data = data.view(-1, 28 * 28).to(device)
+            target = target.to(device)
             model.release_clamp()
             model.reset(batch_size=len(data))
             for _ in range(n_iter):
-                output = model(data)
+                output = model(data.view(-1, 28 * 28))
                 model.backward()
             test_loss += F.nll_loss(
                 F.log_softmax(output, dim=1), target, reduction="sum"
@@ -50,7 +50,8 @@ def test(model, device, test_loader, n_iter=20):
     test_loss /= len(test_loader.dataset)
 
     print(
-        "\nTest set performance: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+        "\nTest set performance:"
+        "Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
             test_loss,
             correct,
             len(test_loader.dataset),
@@ -110,12 +111,13 @@ parser.add_argument(
     "--seed", type=int, default=1, metavar="S", help="random seed (default: 1)"
 )
 args = parser.parse_args()
+
+# Torch configuration
 use_cuda = not args.no_cuda and torch.cuda.is_available()
-
 torch.manual_seed(args.seed)
-
 device = torch.device("cuda" if use_cuda else "cpu")
 
+# Load the dataset
 train_kwargs = {"batch_size": args.batch_size}
 test_kwargs = {"batch_size": args.test_batch_size}
 if use_cuda:
@@ -123,85 +125,14 @@ if use_cuda:
     train_kwargs.update(cuda_kwargs)
     test_kwargs.update(cuda_kwargs)
 
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-    ]
-)
+transform = transforms.ToTensor()
 dataset1 = datasets.MNIST("./data", train=True, download=True, transform=transform)
 dataset2 = datasets.MNIST("./data", train=False, transform=transform)
 train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
 test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-
-class PCModel(nn.Module):
-    def __init__(self, batch_size=64):
-        super().__init__()
-        self.batch_size = batch_size
-        self.layers = nn.Sequential(
-            OrderedDict(
-                [
-                    ("input", InputLayer(batch_size=batch_size, n_units=28 * 28)),
-                    (
-                        "hidden1",
-                        MiddleLayer(batch_size=batch_size, n_units=300, n_in=28 * 28),
-                    ),
-                    (
-                        "hidden2",
-                        MiddleLayer(batch_size=batch_size, n_units=100, n_in=300),
-                    ),
-                    (
-                        "output",
-                        OutputLayer(batch_size=batch_size, n_in=100, n_units=10),
-                    ),
-                ]
-            )
-        )
-
-    def clamp(self, data=None, target=None):
-        if data is not None:
-            data = data.reshape(-1, 28 * 28)
-            self.layers.input.clamp(data)
-        if target is not None:
-            target = F.one_hot(target, 10).float()
-            self.layers.output.clamp(target)
-
-    def release_clamp(self):
-        self.layers.input.release_clamp()
-        self.layers.output.release_clamp()
-
-    def forward(self, x):
-        if x is not None:
-            x = x.reshape(-1, 28 * 28)
-        bu_err = self.layers.input(x)
-        bu_err = self.layers.hidden1(bu_err)
-        bu_err = self.layers.hidden2(bu_err)
-        return self.layers.output(bu_err)
-
-    def backward(self):
-        rec = self.layers.output.backward()
-        rec = self.layers.hidden2.backward(rec)
-        rec = self.layers.hidden1.backward(rec)
-        self.layers.input.backward(rec)
-
-    def train_weights(self, x, lr=0.01):
-        bu_err_hidden1 = self.layers.input(x.reshape(-1, 28 * 28))
-        bu_err_hidden2 = self.layers.hidden1(bu_err_hidden1)
-        bu_err_output = self.layers.hidden2(bu_err_hidden2)
-        self.layers.hidden1.train_weights(bu_err_hidden1, lr=lr)
-        self.layers.hidden2.train_weights(bu_err_hidden2, lr=lr)
-        self.layers.output.train_weights(bu_err_output, lr=lr)
-
-    def reset(self, batch_size=None):
-        if batch_size is not None:
-            self.batch_size = batch_size
-        self.layers.input.reset(batch_size)
-        self.layers.hidden1.reset(batch_size)
-        self.layers.hidden2.reset(batch_size)
-        self.layers.output.reset(batch_size)
-
-
-model = PCModel().to(device)
+# Build and train the model
+model = PCModel(n_units=[28 * 28, 300, 100, 10]).to(device)
 lr = args.lr
 for epoch in range(args.epochs):
     if epoch % args.step_down == 0:
@@ -211,13 +142,13 @@ for epoch in range(args.epochs):
 
 # Save trained model
 checkpoint = dict(args.__dict__)
-checkpoint['state_dict'] = model.state_dict()
-torch.save(checkpoint, 'data/MNIST/trained_model.pkl')
+checkpoint["state_dict"] = model.state_dict()
+torch.save(checkpoint, "data/MNIST/trained_model.pkl")
 
 # Plot reconstruction of the digits
 model.reset(batch_size=10)
 model.release_clamp()
-model.clamp(target=torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).to(device))
+target = F.one_hot(torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), 10).float().to(device)
 recons = []
 for i in range(20):
     output = model(None)
--- torch_predcoding.py
+++ torch_predcoding.py
@@ -7,6 +7,8 @@ These module define both a ``forward`` and ``backward`` method. First, the ``for
 methods should be called in sequence, followed by calling all the ``backward`` methods
 in reverse sequence.
 """
+from collections import OrderedDict
+
 import torch
 from torch import nn
 
@@ -156,6 +158,8 @@ class MiddleLayer(nn.Module):
         ----------
         bu_err : tensor (batch_size, n_in)
             The bottom-up error computed in the previous layer.
+        lr : float
+            The learning rate
         """
         delta = self.state.T @ (bu_err - 1)
         delta /= torch.maximum(self.eps_2, self.state.sum(axis=0, keepdims=True)).T
@@ -388,9 +392,136 @@ class OutputLayer(nn.Module):
         ----------
         bu_err : tensor (batch_size, n_in)
             The bottom-up error computed in the previous layer.
+        lr : float
+            The learning rate.
         """
         delta = self.state.T @ (bu_err - 1)
         delta /= torch.maximum(self.eps_2, self.state.sum(axis=0, keepdims=True)).T
         delta = 1 + lr * delta
         weights = torch.clamp(self.td_weights * delta, 0, 1)
         self.td_weights.set_(weights)
+
+
+class PCModel(nn.Module):
+    """A full predictive coding model.
+
+    Parameters
+    ----------
+    n_units : list of int
+        For each layer, the number of units in that layer. There should be at least two
+        layers: the input and output layers.
+    batch_size : int
+        The batch size used during operation of the model.
+    """
+
+    def __init__(self, n_units, batch_size=64):
+        super().__init__()
+        self.batch_size = batch_size
+
+        assert len(n_units) >= 2, "The model must have at least 2 layers (input/output)"
+        layers = list()
+        layers.append(("input", InputLayer(batch_size=batch_size, n_units=n_units[0])))
+        for i, n in enumerate(n_units[1:-1], 1):
+            layers.append(
+                (
+                    f"hidden{i}",
+                    MiddleLayer(batch_size=batch_size, n_in=n_units[i - 1], n_units=n),
+                )
+            )
+        layers.append(
+            (
+                "output",
+                OutputLayer(
+                    batch_size=batch_size, n_in=n_units[-2], n_units=n_units[-1]
+                ),
+            )
+        )
+        self.layers = nn.Sequential(OrderedDict(layers))
+
+    def clamp(self, input_data=None, output_data=None):
+        """Clamp input/output units unto a given state.
+
+        Parameters
+        ----------
+        input_data: tensor (batch_size, n_in) | None
+            The data to be clamped to the input layer. If left at ``None``, do not clamp
+            the input layer to anything.
+        output_data: tensor (batch_size, n_out) | None
+            The data to be clamped to the output layer. If left at ``None``, do not
+            clamp the output layer to anything.
+        """
+        if input_data is not None:
+            self.layers.input.clamp(input_data)
+        if output_data is not None:
+            self.layers.output.clamp(output_data)
+
+    def release_clamp(self):
+        """Release any clamps on the input and output units."""
+        self.layers.input.release_clamp()
+        self.layers.output.release_clamp()
+
+    def forward(self, input_data=None):
+        """Perform a forward pass throught the model.
+
+        Parameters
+        ----------
+        input_data: tensor (batch_size, n_in) | None
+            The data to be clamped to the input layer during the forward pass. If left
+            at ``None``, do not clamp the input layer to anything.
+
+        Returns
+        -------
+        output_data: tensor (batch_size, n_out)
+            The state of the output units in the model.
+        """
+        bu_err = self.layers.input(input_data)
+        for layer in self.layers[1:-1]:
+            bu_err = layer(bu_err)
+        output_data = self.layers.output(bu_err)
+        return output_data
+
+    def backward(self):
+        """Perform a backward pass through the model.
+
+        Returns
+        -------
+        reconstruction: tensor (batch_size, n_in)
+            The reconstruction of the input units made by the upper layers.
+        """
+        reconstruction = self.layers.output.backward()
+        for layer in self.layers[-2::-1]:
+            reconstruction = layer.backward(reconstruction)
+        return reconstruction
+
+    def train_weights(self, input_data, lr=0.01):
+        """Perform a training step, updating the weights.
+
+        For training to work properly, make sure to have the desired target output
+        clamped onto the output nodes before calling this.
+
+        Parameters
+        ----------
+        input_data: tensor (batch_size, n_in)
+            The data to be clamped to the input layer during the weight training pass.
+            If left at ``None``, do not clamp the input layer to anything.
+        lr : float
+            The learning rate to use when updating weights.
+        """
+        bu_errors = [self.layers.input(input_data)]
+        for layer in self.layers[1:-1]:
+            bu_errors.append(layer(bu_errors[-1]))
+        for layer, bu_err in zip(self.layers[1:], bu_errors):
+            layer.train_weights(bu_err, lr=lr)
+
+    def reset(self, batch_size=None):
+        """Reset the state of all units to small randomized values.
+
+        Parameters
+        ----------
+        batch_size : int | None
+            Optionally change the batch size used by the model.
+        """
+        if batch_size is not None:
+            self.batch_size = batch_size
+        for layer in self.layers:
+            layer.reset(batch_size)
